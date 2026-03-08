a00bae0418b52b24ca6af41a509eaa1bf492648f
Om Bhokare <ombhokare29@gmail.com>
2026-03-09 04:29:47 +0530

    My first code push to github

---
+++ add.py
@@ -0,0 +1,11 @@
+def Addition(a,b):
+    sum = a+b
+    print(a+b)
+    return sum
+
+
+num1 = int(input("Fist number"))
+num2 = int(input("Secong number"))
+result = Addition(num1,num2)
+
+print(result)
