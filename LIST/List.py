f4975b36f467a51e9c3a80e9f38551642641be55
Om Bhokare <ombhokare29@gmail.com>
2026-03-23 23:47:37 +0530

    lists

--- LIST/List.py
+++ LIST/List.py
@@ -8,3 +8,46 @@ print(type(marks))
 
 print(len(marks))
 print(marks[0],marks[1])
+
+# we can store data with different datatypes
+
+
+student = ['name', 12, 12.00]
+
+print(student)
+
+# strings are immutable in python
+# LIST are mutable in python 
+
+student[0] = 'Om'
+
+print(student)
+
+
+# if we try to access the more than the available index it will throw error
+
+
+
+# ************************************************************
+
+
+# *****  LIST SLICING     ************************
+
+mark = [12,3,55,64,7,6,4,34,3]
+print(mark[1:5])
+
+
+
+
+# *****  LIST Methods     ************************
+
+
+
+list = [1,3,2,5]
+
+list.append(4)  #adds 4 at the last 
+
+list.sort() # sorts by ascending order
+
+
+print(list)
