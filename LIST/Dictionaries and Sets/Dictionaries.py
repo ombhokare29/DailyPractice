8a24b2530075d94ed75f60f7639442141acde535
Om Bhokare <ombhokare29@gmail.com>
2026-03-24 22:45:30 +0530

    dicts

---
+++ LIST/Dictionaries and Sets/Dictionaries.py
@@ -0,0 +1,46 @@
+# dictionaries stores the key value pairs
+
+# key:value
+
+# unordered, mutable band no duplicate keys
+
+info  = {
+    "name" : "om",
+    "id" : 12
+}
+print(info)
+
+print(info["name"])
+
+info["name"] = "Raja"
+info["surname"] = "singh"
+
+print(info["name"])
+print(info["surname"])
+
+
+print("*****************************************************\n\n")
+
+
+
+#  nested dictionaries
+
+school = {
+    "name" : "om",
+    "Subject" : {
+        "chem" : 32,
+        "math" : 100,
+        "Phy" : 59
+    }
+}
+
+print(school)
+print(school["Subject"])
+print(school["Subject"]["math"])
+
+
+print("*****************************************************\n\n")
+
+print(school.keys())
+
+print(list(school.keys()))
