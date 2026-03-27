30164fc45be079033fabcc8c4465bdd313a8b255
Om Bhokare <ombhokare29@gmail.com>
2026-03-27 23:19:47 +0530

    Files

---
+++ Files/1.py
@@ -0,0 +1,23 @@
+# types of files
+# 1. text files
+# 2. binary files
+
+# both at the end stores as bits i.e 0 and 1
+
+f = open("/Users/ombhokare/DailyPractice/Files/sample.txt","r")
+
+
+
+data = f.read()
+print(data)
+# line1 = f.readline()
+breakpoint
+# print(line1)
+
+# line2 = f.readline()
+# always a read a line once and pust the pointer at the end of the line
+
+# f.write("This line is new line added by me \n")
+
+# data = f.read()
+# print(data)
