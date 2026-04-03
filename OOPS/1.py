1169c2a815789d6cf2582623e437342d25438979
Om Bhokare <ombhokare29@gmail.com>
2026-04-03 21:40:12 +0530

    hj

---
+++ OOPS/1.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+# simple in-memory storage
+users = []
+
+
+class User(BaseModel):
+    name: str
+    age: int
+
+
+@app.post("/users")
+def create_user(user: User):
+    if user.age < 18:
+        return {"error": "User must be 18+"} 
+
+    new_user = {
+        "id": len(users) + 1,
+        "name": user.name,
+        "age": user.age
+    }
+
+    users.append(new_user)
+    return {"data": new_user}
+
+
+@app.get("/users")
+def get_users():
+    return {"data": users}
